87ab28e7893f948d191363af52cc5aa57ed15858
Niclas Holmquist Jensen <nicl2445@edu.eal.dk>
2019-11-26 09:16:06 +0100

    logging / models

--- VitecMvSemester3/Controllers/HomeController.cs
+++ VitecMvSemester3/Controllers/HomeController.cs
@@ -4,12 +4,31 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using VitecMvSemester3.Models;
 
 namespace VitecMvSemester3.Controllers
 {
     public class HomeController : Controller
     {
+        //--------Logger start--------
+        private readonly ILogger _logger;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult index()
+        {
+            _logger.LogInformation("Log message in the Index() method");
+            return View();
+        }
+        public IActionResult privacy()
+        {
+            _logger.LogInformation("Log message in the Privacy() method");
+            return View();
+        }
+        //------Logger slut--------
+
         public IActionResult Index()
         {
             return View();
---
+++ VitecMvSemester3/Models/Produkt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VitecMvSemester3.Models
+{
+    public class Produkt
+    {
+        public string Navn { get; set; }
+        public int Pris { get; set; }
+        public string Type { get; set; }
+        public string Beskrivelse { get; set; }
+
+    }
+}
